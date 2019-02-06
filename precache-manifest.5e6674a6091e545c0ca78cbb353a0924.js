6b1b1d0dbe67d3952162356e87883172fa935c7f
Steven Boutcher <sjboutcher@gmail.com>
2019-02-05 20:59:02 -0600

    Updates

---
+++ precache-manifest.5e6674a6091e545c0ca78cbb353a0924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fcc-fel1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "24fc77fee6310eccfb018e7f79219e12",
+    "url": "/fcc-fel1/static/media/background.24fc77fe.24fc77fe.png"
+  },
+  {
+    "revision": "c019ccb2f2186183d551",
+    "url": "/fcc-fel1/static/js/runtime~main.c019ccb2.js"
+  },
+  {
+    "revision": "5d86d27aeb18101c59f4",
+    "url": "/fcc-fel1/static/js/main.5d86d27a.chunk.js"
+  },
+  {
+    "revision": "4507bae7b50970c119a4",
+    "url": "/fcc-fel1/static/js/1.4507bae7.chunk.js"
+  },
+  {
+    "revision": "5d86d27aeb18101c59f4",
+    "url": "/fcc-fel1/static/css/main.684d18b8.chunk.css"
+  },
+  {
+    "revision": "17e94dc8d99ef1dc3ed7d7bd899bdb57",
+    "url": "/fcc-fel1/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/fcc-fel1/precache-manifest.5e6674a6091e545c0ca78cbb353a0924.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/fcc-fel1/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
