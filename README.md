da03006218d85d2fb7bdedde75472982fb7adc86
Steven Boutcher <sjboutcher@gmail.com>
2019-06-13 09:21:30 -0500

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# fcc-fel1
-1st project for freeCodeCamp's Front End Libraries Certification -- React Random Quote Generator
---
+++ precache-manifest.2007d1bd493ebf8317deaa57a6d59f6f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "24fc77fee6310eccfb018e7f79219e12",
+    "url": "/fcc-fel1/static/media/background.24fc77fe.24fc77fe.png"
+  },
+  {
+    "revision": "28b3e0f7a059fa8b43b9",
+    "url": "/fcc-fel1/static/css/main.cd09ceb7.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/fcc-fel1/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "deb6d5424d5873f29a3b",
+    "url": "/fcc-fel1/static/js/1.deb6d542.chunk.js"
+  },
+  {
+    "revision": "c019ccb2f2186183d551",
+    "url": "/fcc-fel1/static/js/runtime~main.c019ccb2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fcc-fel1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "28b3e0f7a059fa8b43b9",
+    "url": "/fcc-fel1/static/js/main.28b3e0f7.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fcc-fel1/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fcc-fel1/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fcc-fel1/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fcc-fel1/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "deb6d5424d5873f29a3b",
+    "url": "/fcc-fel1/static/css/1.6d7749eb.chunk.css"
+  },
+  {
+    "revision": "8fda00920eed5527dfc068fc4c31d452",
+    "url": "/fcc-fel1/index.html"
+  }
+];
